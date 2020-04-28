f1bd82f8dad89f5a7619a40da7cef60757343e1c
Elena Lee <jelee.elena@gmail.com>
2020-04-28 12:08:00 +0900

    complete helpers

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -1,9 +1,22 @@
-const https = require('https');
+const https = require("https");
 
 async function retrieveArticle(url) {
-  // TODO: retrieve the html string from given url and return as promise
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve(data);
+      });
+      res.on("error", (err) => {
+        reject(err);
+      });
+    });
+  });
 }
 
 module.exports = {
-  retrieveArticle
+  retrieveArticle,
 };
--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -2,12 +2,25 @@ const fs = require("fs");
 
 async function writeFile(filename, body) {
   return new Promise((resolve, reject) => {
-    // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
+    fs.writeFile(filename, body, "utf8", (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
   });
 }
 
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
+    fs.readFile(filename, "utf8", (err, content) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(content);
+      }
+    });
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
   });
 }
@@ -22,14 +35,19 @@ async function writeSourceListFile(body) {
 
 async function readLineFromSourceList(nthline) {
   return new Promise((resolve, reject) => {
+    readSourceListFile()
+      .then((data) => data.split("\n"))
+      .then((arr) => resolve(arr[nthline]))
+      .catch((err) => reject(err));
     // TODO : ./data/source.txt에 저장되어 있는 텍스트에서 특정 줄에 해당하는 텍스트를 읽을 수 있도록 구현하세요.
   });
 }
+// 'helloMocking\nhelloMocha\nhelloJavaScript'
 
 module.exports = {
   readSourceListFile,
   writeSourceListFile,
   writeFile,
   readFile,
-  readLineFromSourceList
+  readLineFromSourceList,
 };
