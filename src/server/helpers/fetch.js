e4edc2641de4979f00d3508833b29e52d09127a7
unknown <songal4546@gmail.com>
2020-04-28 14:33:55 +0900

    not yet

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -2,8 +2,22 @@ const https = require('https');
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let body = '';
+      res.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+      res.on('end', () => {
+        resolve(body);
+      });
+      res.on('error', (err) => {
+        reject(err);
+      });
+    });
+  });
 }
 
 module.exports = {
-  retrieveArticle
+  retrieveArticle,
 };
--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -1,28 +1,46 @@
-const fs = require("fs");
+const fs = require('fs');
 
 async function writeFile(filename, body) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
+    fs.writeFile(filename, body, 'utf8', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
   });
 }
 
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile(filename, 'utf8', (err, content) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(content);
+      }
+    });
   });
 }
 
 async function readSourceListFile() {
-  return readFile("./data/source.txt");
+  return readFile('./data/source.txt');
 }
 
 async function writeSourceListFile(body) {
-  return writeFile("./data/source.txt", body);
+  return writeFile('./data/source.txt', body);
 }
 
 async function readLineFromSourceList(nthline) {
   return new Promise((resolve, reject) => {
     // TODO : ./data/source.txt에 저장되어 있는 텍스트에서 특정 줄에 해당하는 텍스트를 읽을 수 있도록 구현하세요.
+    readSourceListFile()
+      .then((data) => data.split('\n'))
+      .then((data) => resolve(data[nthline]))
+      .catch((err) => reject(err));
   });
 }
 
@@ -31,5 +49,5 @@ module.exports = {
   writeSourceListFile,
   writeFile,
   readFile,
-  readLineFromSourceList
+  readLineFromSourceList,
 };
