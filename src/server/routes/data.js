19e88ace617291191ccee43f728e4f1a213d6f5d
unknown <songal4546@gmail.com>
2020-04-29 20:59:17 +0900

    done

--- src/server/routes/data.js
+++ src/server/routes/data.js
@@ -1,28 +1,28 @@
 /* eslint-disable no-console */
-const express = require("express");
-const fs = require("fs");
-const jsdom = require("jsdom");
+const express = require('express');
+const fs = require('fs');
+const jsdom = require('jsdom');
 
-const fetchHelper = require("../helpers/fetch");
-const fileHelper = require("../helpers/file");
+const fetchHelper = require('../helpers/fetch');
+const fileHelper = require('../helpers/file');
 
 const router = express.Router();
 const { JSDOM } = jsdom;
 
 // GET /data/{lineNo}
-router.get("/:line", async (req, res) => {
+router.get('/:line', async (req, res) => {
   const filename = `./data/${req.params.line}.txt`;
-  res.set("Content-Type", "application/json");
+  res.set('Content-Type', 'application/json');
   if (fs.existsSync(filename)) {
     fileHelper
       .readFile(filename)
       .then((data) =>
-        res.status(200).send({ id: req.params.line, body: data, status: "ok" })
+        res.status(200).send({ id: req.params.line, body: data, status: 'ok' })
       );
   } else {
     res
       .status(200)
-      .send({ id: req.params.line, body: null, status: "nonexist" });
+      .send({ id: req.params.line, body: null, status: 'nonexist' });
   }
 
   // TODO : Help function을 이용하여, 주어진 filename의 내용을 읽을 수 있도록 구현하세요.
@@ -32,15 +32,15 @@ router.get("/:line", async (req, res) => {
 });
 
 // POST /data/{lineNo}
-router.post("/:line", async (req, res) => {
+router.post('/:line', async (req, res) => {
   const lineNo = req.params.line;
   const url = await fileHelper.readLineFromSourceList(lineNo);
   const article = await fetchHelper.retrieveArticle(url);
   const dom = new JSDOM(article);
-  const body = dom.window.document.querySelector("body").textContent;
+  const body = dom.window.document.querySelector('body').textContent;
   fileHelper.writeFile(`./data/${lineNo}.txt`, body);
 
-  res.status(200).send("ok");
+  res.status(200).send('ok');
 
   // TODO : Help function을 이용하여, 주어진 filename에 내용을 저장할 수 있도록 구현하세요.
   /*
